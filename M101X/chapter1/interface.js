183360a352bf0615bfdd3a7e33987a746bfd05d6
JOSE RAFAEL RAMON NAVARRO <jrafa.ramon@gmail.com>
2016-01-09 11:46:48 +0100

    M101X - Chapter 1 Homeworks Solved

--- M101X/chapter1/interface.js
+++ M101X/chapter1/interface.js
@@ -1,9 +1,12 @@
 /*
  *  Inserts "doc" into the collection "movies".
  */
+
 exports.insert = function(db, doc, callback) {
-  // TODO: implement
-  callback(null);
+  	db.collection('movies').insert(doc, function(error,result){ 
+		if (error){ callback(error); } 
+		else{ callback(null); } 
+  	}); 
 };
 
 /*
@@ -12,7 +15,13 @@ exports.insert = function(db, doc, callback) {
  *  ordered by the movie's "title" field. See
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
+
+var director = 'Irvin Kershner';
 exports.byDirector = function(db, director, callback) {
-  // TODO: implement
-  callback(null, []);
+	db.collection('movies')
+	.find({'director':director})
+	.sort({'title':1})
+	.toArray(function(error,docs){
+ 		if (error){ console.log(error); } 
+		else{ callback(null, docs); } });
 };
